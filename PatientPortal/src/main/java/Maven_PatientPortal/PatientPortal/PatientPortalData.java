5a6b96f5c19982e11b761f752a2b3047d7ddebd0
Ankit Mathur <ankit.mathur31@gmail.com>
2019-01-27 12:00:21 -0500

    Added PatientPortalData.java

---
+++ PatientPortal/src/main/java/Maven_PatientPortal/PatientPortal/PatientPortalData.java
@@ -0,0 +1,37 @@
+//This class is for data initialization.
+
+package Maven_PatientPortal.PatientPortal;
+
+import org.openqa.selenium.WebDriver;
+
+public class PatientPortalData 
+{
+	//Login data
+			public WebDriver driver = null;
+			public static String url = null;
+			public static String username = null;
+			public static String password = null;
+			
+	//Registration data
+			public static String firstname = null;
+			public static String middlename = null;
+			public static String lastname = null;
+			public static String gender = null;
+			public static String date = null;
+			public static String month = null;
+			public static String year = null;
+			
+			public static String address1 = null;
+			public static String address2 = null;
+			public static String city = null;
+			public static String state = null;
+			public static String country = null;
+			public static String zipcode = null;
+			
+			public static String phoneNumber = null;
+			
+			public static String relationType = null;
+			public static String relationName = null;
+}
+
+
